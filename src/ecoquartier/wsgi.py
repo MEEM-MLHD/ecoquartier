dd3f2099ea45e05db5385ae27640993f6ddf0578
ouhouhsami <samuel.goldszmidt@gmail.com>
2016-08-31 17:35:10 +0200

    update wsgi

--- src/ecoquartier/wsgi.py
+++ src/ecoquartier/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
+#!/usr/bin/python
 import os
+import sys
+
+here = os.path.dirname(__file__)
+sys.path.append(os.path.dirname(here))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecoquartier.settings")
-
 application = get_wsgi_application()

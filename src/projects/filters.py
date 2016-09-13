20db9d073c2536dd0c3867da9fa4d7afcdff5dc4
ouhouhsami <samuel.goldszmidt@gmail.com>
2016-09-13 18:07:41 +0200

    remove some filters #2

--- src/projects/filters.py
+++ src/projects/filters.py
@@ -6,4 +6,4 @@ from projects.models import Project
 class ProjectFilter(django_filters.FilterSet):
     class Meta:
         model = Project
-        fields = ['statut', 'zonage_insee', 'departement', 'region', 'contexte_commune', 'type_operation', 'vocation', 'label_ecoquartier', 'procedure', ]
+        fields = ['zonage_insee', 'type_operation', 'label_ecoquartier']

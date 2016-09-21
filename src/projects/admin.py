a19251f0c5acc15872e278041d729335cf6bb95c
ouhouhsami <samuel.goldszmidt@gmail.com>
2016-09-21 16:49:42 +0200

    add Charte Model

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 
-from .models import Project, Statut, ZonageINSEE, Commune, Departement, Region, ContexteCommune, TypeOperation, Vocation, LabelEcoQuartier, Procedure
+from .models import Project, Statut, ZonageINSEE, Commune, Departement, Region, ContexteCommune, TypeOperation, Vocation, LabelEcoQuartier, Procedure, Charte
+
+class CharteAdmin(admin.ModelAdmin):
+    list_display = ('region', 'ville', 'nom_ecoquartier')
 
 
 admin.site.register(Project)
@@ -15,3 +18,4 @@ admin.site.register(TypeOperation)
 admin.site.register(Vocation)
 admin.site.register(LabelEcoQuartier)
 admin.site.register(Procedure)
+admin.site.register(Charte, CharteAdmin)
